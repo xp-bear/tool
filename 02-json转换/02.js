e6fd7e45fdb84695cdef49c4996f20045f6f7896
想走过亚洲的熊 <1693889638@qq.com>
2024-07-05 20:48:13 +0800

    新增json格式在线转换

---
+++ 02-json转换/02.js
@@ -0,0 +1,41 @@
+let inner = document.querySelector(".inner");
+let out = document.querySelector(".out");
+inner.addEventListener("input", function (e) {
+  //   console.log(this.value); //实时监听的值
+
+  let arr = this.value.split("\n"); //先按行分割
+  let obj = {}; //转化为一个对象
+  arr.forEach((item) => {
+    let headarr = item.split(": ");
+    obj[headarr[0]] = headarr[1];
+  });
+  let originStr = JSON.stringify(obj);
+  originStr = originStr.replace("{", "");
+  originStr = originStr.substring(0, originStr.length - 1);
+  // 使用正则表达式找到所有的逗号，并在每个逗号后面添加换行符
+  let handleStr = originStr.replace(/","/g, '",\n"');
+  // console.log(handleStr);
+
+  out.value = handleStr;
+});
+// 复制文字函数
+async function copyTextToClipboard(text) {
+  try {
+    // 等待navigator.clipboard变得可用
+    await navigator.clipboard.writeText(text);
+    console.log("文本已成功复制到剪贴板");
+  } catch (err) {
+    console.error("无法复制文本: ", err);
+  }
+}
+// 点击copy按钮
+let copy = document.querySelector(".copy");
+copy.addEventListener("click", function () {
+  // 使用示例
+  console.log(out.value);
+  if (out.value.length <= 0) {
+    return alert("输出文本为空,请先转换!");
+  }
+  copyTextToClipboard(out.value);
+  alert("复制成功!");
+});
